ea1bb3fb25fb7b7bdd8d0b30a89dc1f6a5ccf9a5
Greg Wilson <gvwilson@third-bit.com>
2022-09-06 08:23:21 -0400

    Internationalizing credits

--- lib/mccole/extensions/credits.py
+++ lib/mccole/extensions/credits.py
@@ -13,6 +13,8 @@ def bibliography(pargs, kwargs, node):
     util.require((not pargs) and (not kwargs), "Bad 'credits' shortcode")
     if (filename := ivy.site.config.get("credits", None)) is None:
         return '<p class="warning">No credits specified.</p>'
+    if (lang := ivy.site.config.get("lang", None)) is None:
+        return '<p class="warning">No language specified.</p>'
     with open(filename, "r") as reader:
         credits = yaml.safe_load(reader)
-    return "\n\n".join([e["bio"] for e in credits])
+    return "\n\n".join([e[lang] for e in credits])
