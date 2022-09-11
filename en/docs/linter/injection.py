a8fca50a53dbe5279298a494ffaa485a56f21d90
Greg Wilson <gvwilson@third-bit.com>
2022-09-11 10:06:53 -0400

    Adding one last example to the linter chapter

---
+++ en/docs/linter/injection.py
@@ -0,0 +1,20 @@
+import ast
+import sys
+
+# [attach]
+class BlankNodeVisitor(ast.NodeVisitor):
+    pass
+
+def print_name(self, node):
+    print(node.id)
+
+setattr(BlankNodeVisitor, "visit_Name", print_name)
+# [/attach]
+
+# [main]
+with open(sys.argv[1], "r") as reader:
+    source = reader.read()
+tree = ast.parse(source)
+finder = BlankNodeVisitor()
+finder.visit(tree)
+# [/main]
