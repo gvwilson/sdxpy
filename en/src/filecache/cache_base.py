5758537112e78a0a829eed1eab449d57c674c9af
Greg Wilson <gvwilson@third-bit.com>
2022-09-12 16:23:42 -0400

    More refactoring

--- en/src/filecache/cache_base.py
+++ en/src/filecache/cache_base.py
@@ -9,19 +9,49 @@ class CacheException(Exception):
 class CacheBase(ABC):
     """Make files locally available."""
 
-    def __init__(self, local_dir, local_size):
+    def __init__(self, index, local_dir, local_size):
         """Initialize cache."""
+        self.index = index
         self.local_dir = local_dir
         self.local_size = local_size
 
     @abstractmethod
-    def has(self, identifier, local_only=False):
-        """Is this file available (locally)?"""
-
-    @abstractmethod
-    def get_local(self, identifier, download=True):
-        """Get the path to a local copy of the file or raise an exception."""
-
-    @abstractmethod
     def add(self, identifier, local_path, update=False):
         """Add a file to the system, replacing existing if told to do so."""
+
+    @abstractmethod
+    def _localize_file(self, identifier, local_path):
+        """Get a local copy of a file."""
+
+    def has(self, identifier, local_only=False):
+        """Is this file available (locally)?"""
+        if not self.index.has(identifier):
+            return False
+        if local_only:
+            local_path = self._make_local_path(identifier)
+            return local_path.exists()
+        return True
+
+    def get_local_path(self, identifier, download=True):
+        """Get the path to a local copy of the file or raise an exception."""
+        if not self.index.has(identifier):
+            raise CacheException(f"Unknown file identifier {identifier}")
+        local_path = self._make_local_path(identifier)
+        if not local_path.exists():
+            self._localize_file(identifier, local_path)
+        return local_path
+
+    def _cleanup(self, but_not=None):
+        """Clean up the local cache."""
+        for identifier in index.least_recently_used():
+            if shutil.disk_usage(self.local_dir) <= self.local_size:
+                break
+            if (but_not is not None) and (identifier == but_not):
+                continue
+            delete_path = self._make_local_path(identifier)
+            delete_path.unlink()
+
+    def _make_local_path(self, identifier):
+        """Construct the path to a localized file."""
+        return Path(self.local_dir, f"{identifier}.cache")
+
--- en/src/filecache/cache_filesystem.py
+++ en/src/filecache/cache_filesystem.py
@@ -1,19 +1,24 @@
+from pathlib import Path
+import shutil
+
 from cache_base import CacheBase, CacheException
 
 
 class CacheFilesystem(CacheBase):
     """Cache files using local filesystem."""
 
-    def __init__(self, master_dir, local_dir, local_size):
+    def __init__(self, index, remote_dir, local_dir, local_size):
         """Initialize cache."""
-        super().__init__(local_dir, local_size)
-        self.master_dir = master_dir
-
-    def has(self, identifier, local_only=False):
-        """Is this file available (locally)?"""
-
-    def get_local(self, identifier, download=True):
-        """Get the path to a local copy of the file or raise an exception."""
+        super().__init__(index, local_dir, local_size)
+        self.remote_dir = remote_dir
 
     def add(self, identifier, local_path, update=False):
         """Add a file to the system, replacing existing if told to do so."""
+
+    def _localize_file(self, identifier, local_path):
+        """Get a local copy of the file."""
+        remote_path = self._make_remote_path(identifier)
+        if not remote_path.exists():
+            raise CacheException(f"Remote file {identifier} at {remote_path} does not exist")
+        shutil.copyfile(remote_path, local_path)
+        self._cleanup(but_not=identifier)
