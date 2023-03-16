d2207a319ea30eaeb8669c51993db24a700c72e9
Greg Wilson <gvwilson@third-bit.com>
2023-03-15 20:53:27 -0400

    Matching command-line globbing

---
+++ en/src/matching/glob_either.py
@@ -0,0 +1,8 @@
+class Either:
+    def __init__(self, left, right, rest=None):
+        self.left = left
+        self.right = right
+        self.rest = rest
+
+    def match(self, text, start=0):
+        return self.left.match(text, start) or self.right.match(text, start)
