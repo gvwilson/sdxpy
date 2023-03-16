5c48430d02e298796a49707f01c7176f9e8393a9
Greg Wilson <gvwilson@third-bit.com>
2023-03-16 19:38:35 -0400

    Using the Null Object pattern

--- en/src/matching/test_glob_any.py
+++ en/src/matching/test_glob_any.py
@@ -1,22 +1,22 @@
 from glob_lit import Lit
 from glob_any import Any
 
-def test_star_matches_empty():
+def test_any_matches_empty():
     # ⌈*⌋ ≈ ""
     assert Any().match("")
 
-def test_star_matches_entire_string():
+def test_any_matches_entire_string():
     # ⌈*⌋ ≈ "abc"
     assert Any().match("abc")
 
-def test_star_matches_as_prefix():
+def test_any_matches_as_prefix():
     # ⌈*def⌋ ≈ "abcdef"
     assert Any(Lit("def")).match("abcdef")
 
-def test_star_matches_as_suffix():
+def test_any_matches_as_suffix():
     # ⌈abc*⌋ ≈ "abcdef"
     assert Lit("abc", Any()).match("abcdef")
 
-def test_star_matches_interior():
+def test_any_matches_interior():
     # ⌈a*c⌋ ≈ "abc"
     assert Lit("a", Any(Lit("c"))).match("abc")
