28b0cd96cab92d674c4f079d17fc65b78af726f9
Greg Wilson <gvwilson@third-bit.com>
2023-03-21 18:25:31 -0400

    Only linking to slide files that exist

--- lib/mccole/extensions/slides.py
+++ lib/mccole/extensions/slides.py
@@ -4,11 +4,10 @@ import ivy
 import shortcodes
 import util
 
-_markdown_links = None
-
 
 @ivy.filters.register(ivy.filters.Filter.FILE_TEXT)
-def add_markdown_list(text, meta_dict):
+def add_markdown_link_list(text, meta_dict):
+    """Add all Markdown links to slides for conversion to HTML."""
     if meta_dict.get("template", None) == "slides":
         text += "\n\n" + util.make_links_table()
     return text
@@ -16,7 +15,7 @@ def add_markdown_list(text, meta_dict):
 
 @shortcodes.register("slides")
 def slide_list(pargs, kwargs, node):
-    """Convert generate list of slides."""
+    """Generate list of slides with links."""
     util.require(
         (not pargs) and (not kwargs),
         f"Bad 'slides' shortcode {pargs} and {kwargs}",
@@ -24,8 +23,14 @@ def slide_list(pargs, kwargs, node):
     titles = util.get_config("titles")
     result = ["<ol>"]
     for entry in titles["chapters"]:
+        if ivy.nodes.node(f"@root/{entry.slug}/slides/"):
+            prefix = f'<a href="@root/{entry.slug}/slides/" markdown="1">'
+            suffix = '</a>'
+        else:
+            prefix = ''
+            suffix = ''
         result.append(
-            f'<li><a href="@root/{entry.slug}/slides/" markdown="1">{entry.title}</a></li>'
+            f'<li>{prefix}{entry.title}{suffix}</li>'
         )
     result.append("</ol>")
     return "\n".join(result)
