9ce1157c82ca29c681783aacb16849659b194349
Greg Wilson <gvwilson@third-bit.com>
2023-04-12 14:39:22 -0400

    Adding feedback markers

--- docs/undo/wasim.py
+++ docs/undo/wasim.py
@@ -2,6 +2,97 @@ import argparse
 import curses
 import sys
 
+
+class Buffer:
+    def __init__(self, lines):
+        self.lines = lines
+
+    def __len__(self):
+        return len(self.lines)
+
+    def __getitem__(self, index):
+        return self.lines[index]
+
+    @property
+    def bottom(self):
+        return len(self) - 1
+
+    def insert(self, cursor, string):
+        row, col = cursor.row, cursor.col
+        current = self.lines.pop(row)
+        new = current[:col] + string + current[col:]
+        self.lines.insert(row, new)
+
+    def split(self, cursor):
+        row, col = cursor.row, cursor.col
+        current = self.lines.pop(row)
+        self.lines.insert(row, current[:col])
+        self.lines.insert(row + 1, current[col:])
+
+    def delete(self, cursor):
+        row, col = cursor.row, cursor.col
+        if (row, col) < (self.bottom, len(self[row])):
+            current = self.lines.pop(row)
+            if col < len(self[row]):
+                new = current[:col] + current[col + 1:]
+                self.lines.insert(row, new)
+            else:
+                next = self.lines.pop(row)
+                new = current + next
+                self.lines.insert(row, new)
+
+
+def clamp(x, lower, upper):
+    if x < lower:
+        return lower
+    if x > upper:
+        return upper
+    return x
+
+
+class Cursor:
+    def __init__(self, row=0, col=0, col_hint=None):
+        self.row = row
+        self._col = col
+        self._col_hint = col if col_hint is None else col_hint
+
+    @property
+    def col(self):
+        return self._col
+
+    @col.setter
+    def col(self, col):
+        self._col = col
+        self._col_hint = col
+
+    def _clamp_col(self, buffer):
+        self._col = min(self._col_hint, len(buffer[self.row]))
+
+    def up(self, buffer):
+        if self.row > 0:
+            self.row -= 1
+            self._clamp_col(buffer)
+
+    def down(self, buffer):
+        if self.row < len(buffer) - 1:
+            self.row += 1
+            self._clamp_col(buffer)
+
+    def left(self, buffer):
+        if self.col > 0:
+            self.col -= 1
+        elif self.row > 0:
+            self.row -= 1
+            self.col = len(buffer[self.row])
+
+    def right(self, buffer):
+        if self.col < len(buffer[self.row]):
+            self.col += 1
+        elif self.row < len(buffer) - 1:
+            self.row += 1
+            self.col = 0
+
+
 class Window:
     def __init__(self, n_rows, n_cols, row=0, col=0):
         self.n_rows = n_rows
@@ -21,49 +112,24 @@ class Window:
         if cursor.row == self.bottom + 1 and self.bottom < len(buffer) - 1:
             self.row += 1
 
-    def translate(self, cursor):
-        result = cursor.row - self.row, cursor.col - self.col
-        return result
-
     def horizontal_scroll(self, cursor, left_margin=5, right_margin=2):
         n_pages = cursor.col // (self.n_cols - right_margin)
         self.col = max(n_pages * self.n_cols - right_margin - left_margin, 0)
 
-class Cursor:
-    def __init__(self, row=0, col=0):
-        self.row = row
-        self.col = col
+    def translate(self, cursor):
+        return cursor.row - self.row, cursor.col - self.col
 
-    def up(self, buffer):
-        if self.row > 0:
-            self.row -= 1
-            self._clamp_col(buffer)
 
-    def down(self, buffer):
-        if self.row < len(buffer) - 1:
-            self.row += 1
-            self._clamp_col(buffer)
+def left(window, buffer, cursor):
+    cursor.left(buffer)
+    window.up(cursor)
+    window.horizontal_scroll(cursor)
 
-    def left(self, buffer):
-        if self.col > 0:
-            self.col -= 1
-        elif self.row > 0:
-            self.row -= 1
-            self.col = max(0, len(buffer[self.row]) - 1)
 
-    def right(self, buffer):
-        if self.col < len(buffer[self.row]) - 1:
-            self.col += 1
-        elif self.row < len(buffer) - 1:
-            self.row += 1
-            self.col = 0
-
-    def _clamp_col(self, buffer):
-        old = self.col
-        self.col = max(0, min(self.col, len(buffer[self.row])-1))
-
-    def __str__(self):
-        return f"r{self.row}c{self.col}"
+def right(window, buffer, cursor):
+    cursor.right(buffer)
+    window.down(buffer, cursor)
+    window.horizontal_scroll(cursor)
 
 
 def main(stdscr):
@@ -72,7 +138,7 @@ def main(stdscr):
     args = parser.parse_args()
 
     with open(args.filename) as f:
-        buffer = [s.rstrip("\n") for s in f.readlines()]
+        buffer = Buffer(f.read().splitlines())
 
     window = Window(curses.LINES - 1, curses.COLS - 1)
     cursor = Cursor()
@@ -90,19 +156,32 @@ def main(stdscr):
         k = stdscr.getkey()
         if k == "q":
             sys.exit(0)
-        elif k == "KEY_UP":
-            cursor.up(buffer)
-            window.up(cursor)
+        elif k == "KEY_LEFT":
+            left(window, buffer, cursor)
         elif k == "KEY_DOWN":
             cursor.down(buffer)
             window.down(buffer, cursor)
-        elif k == "KEY_LEFT":
-            cursor.left(buffer)
+            window.horizontal_scroll(cursor)
+        elif k == "KEY_UP":
+            cursor.up(buffer)
             window.up(cursor)
+            window.horizontal_scroll(cursor)
         elif k == "KEY_RIGHT":
-            cursor.right(buffer)
-            window.down(buffer, cursor)
-        window.horizontal_scroll(cursor)
+            right(window, buffer, cursor)
+        elif k == "\n":
+            buffer.split(cursor)
+            right(window, buffer, cursor)
+        elif k in ("KEY_DELETE", "\x04"):
+            buffer.delete(cursor)
+        elif k in ("KEY_BACKSPACE", "\x7f"):
+            if (cursor.row, cursor.col) > (0, 0):
+                left(window, buffer, cursor)
+                buffer.delete(cursor)
+        else:
+            buffer.insert(cursor, k)
+            for _ in k:
+                right(window, buffer, cursor)
+
 
 if __name__ == "__main__":
     curses.wrapper(main)
