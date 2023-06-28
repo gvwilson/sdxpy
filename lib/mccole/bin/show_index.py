272dc2bdc7f3c8b9543c1779fc31127fb39dd94f
Greg Wilson <gvwilson@third-bit.com>
2023-06-28 07:42:33 -0400

    Showing full index

--- lib/mccole/bin/show_index.py
+++ lib/mccole/bin/show_index.py
@@ -16,29 +16,39 @@ def main():
         slug: open(Path(config.src_dir, slug, "index.md"), "r").read()
         for slug in [*config.chapters.keys(), *config.appendices.keys()]
     }
-    forward = {
-        slug: get_index_keys(text) for (slug, text) in prose.items()
+    glossary = {
+        entry["key"]: entry[config.lang]["term"]
+        for entry in util.read_yaml(config.glossary)
     }
-    for key, slugs in sorted(reverse_dict(forward).items()):
+    forward = {
+        slug: get_index(glossary, text) for (slug, text) in prose.items()
+    }
+    for key, slugs in sorted(reverse_dict(forward).items(), key=lambda x: x[0].lower()):
         print(f"{key}: {', '.join(slugs)}")
 
 
-def get_index_keys(text):
+def get_index(glossary, text):
     """Create set of all index keys in text."""
     parser = shortcodes.Parser(inherit_globals=False, ignore_unknown=True)
-    parser.register(_get_keys, "i", "/i")
-    temp = set()
+    parser.register(_index_keys, "i", "/i")
+    parser.register(_glossary_keys, "g")
+    temp = {"keys": set(), "glossary": glossary}
     try:
         parser.parse(text, temp)
-        return temp
+        return temp["keys"]
     except shortcodes.ShortcodeSyntaxError as exc:
         print(f"%i shortcode parsing error in {node.filepath}: {exc}", file=sys.stderr)
         sys.exit(1)
 
 
-def _get_keys(pargs, kwargs, extra, content):
+def _glossary_keys(pargs, kwargs, extra):
+    """Get keys out of glossary entry."""
+    extra["keys"].add(extra["glossary"][pargs[0]])
+
+
+def _index_keys(pargs, kwargs, extra, content):
     """Get keys out of index entry."""
-    extra.update(key.strip() for key in pargs)
+    extra["keys"].update(key.strip() for key in pargs)
 
 
 def parse_args():
