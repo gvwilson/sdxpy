52e1357bd31677d88ed09ba285e77f85334800ef
Greg Wilson <gvwilson@third-bit.com>
2023-07-06 10:39:40 -0400

    fix: better status reporting

--- lib/mccole/bin/status.py
+++ lib/mccole/bin/status.py
@@ -1,79 +1,70 @@
 #!/usr/bin/env python
 
 import argparse
+from collections.abc import Iterable
+import frontmatter
 from pathlib import Path
-
 import regex
 import util
 from prettytable import MARKDOWN, PrettyTable
 
-EXERCISES_PER = 8
+EXERCISES_PER = 5
+FIGURES_PER = 4
 SLIDES_PER = 18
+SYLLABUS_PER = 5
 WORDS_PER = 2200
 THRESHOLD_LOW = 80
-THRESHOLD_HIGH = 120
-
+THRESHOLD_HIGH = 200
 SHORT_CHAPTERS = {"intro", "finale"}
+HEADINGS = "title slides exercises figures syllabus words".split()
 
 
 def main():
     """Main driver."""
     options = parse_args()
     config = util.load_config(options.config)
-    wrapper = choose_wrapper(options)
-    report(wrapper, config.chapters)
+    report(options.highlight, config.chapters)
 
 
 def build_format(chapters):
-    longest = max(len(s) for s in chapters.keys())
-    longest = max(longest, len("Chapter"))
+    longest = max("title", *[len(s) for s in chapters.keys()])
     fmt = f"{{:{longest}}}"
     return fmt, "-" * longest
 
 
-def calc_fraction(n_slides, n_ex, n_words, slug=None):
+def calculate_fraction(slug, actual, expected):
+    fraction = 100 * actual / expected
     if slug in SHORT_CHAPTERS:
-        entries = ((n_slides, SLIDES_PER / 2), (n_words, WORDS_PER / 2))
-    else:
-        entries = (
-            (n_slides, SLIDES_PER),
-            (n_ex, EXERCISES_PER),
-            (n_words, WORDS_PER),
-        )
-    frac = sum(entry[0] / entry[1] for entry in entries)
-    return int(100 * frac / len(entries))
+        fraction *= 2
+    return fraction
 
 
-def choose_wrapper(options):
-    """Choose how to wrap status figures."""
-    if options.colorize:
-        return colorize
-    if options.markdown:
-        return markdownify
-    return lambda x: f"{x}%"
-
-
-def colorize(fraction):
-    if fraction < THRESHOLD_LOW:
-        return f"{util.RED}{fraction}%{util.ENDC}"
-    elif fraction > THRESHOLD_HIGH:
-        return f"{util.BLUE}{fraction}%{util.ENDC}"
-    else:
-        return f"{util.GREEN}{fraction}%{util.ENDC}"
-
+def create_row(slug, wrap, total):
+    n_slides = count_slides(slug)
+    n_ex, n_fig, n_syll, n_words = count_page(slug)
+    combined = (
+        ("slides", n_slides, SLIDES_PER),
+        ("exercises", n_ex, EXERCISES_PER),
+        ("figures", n_fig, FIGURES_PER),
+        ("syllabus", n_syll, SYLLABUS_PER),
+        ("words", n_words, WORDS_PER),
+    )
+    for (key, val, _) in combined:
+        total[key] += val
+    return [
+        slug,
+        *[wrap(slug, val, expected) for (_, val, expected) in combined]
+    ]
 
 def count_page(slug):
     with open(Path("src", slug, "index.md"), "r") as reader:
         page = reader.read()
         return (
-            count_re(regex.EXERCISE_HEADER, page),
-            count_re(regex.FIGURE, page),
-            count_words(page),
+            len(list(regex.EXERCISE_HEADER.finditer(page))),
+            len(list(regex.FIGURE.finditer(page))),
+            len(frontmatter.loads(page).get("syllabus", [])),
+            len([x for x in page.split() if x]),
         )
-
-
-def count_re(pat, text):
-    return len(list(pat.finditer(text)))
 
 
 def count_slides(slug):
@@ -83,82 +74,91 @@ def count_slides(slug):
         return lines.count("---") - 2
 
 
-def count_words(text):
-    return len([x for x in text.split() if x])
+def highlight_ascii(slug, actual, expected):
+    fraction = calculate_fraction(slug, actual, expected)
+    if fraction < THRESHOLD_LOW:
+        return f"{util.RED}{actual}{util.ENDC}"
+    elif fraction > THRESHOLD_HIGH:
+        return f"{util.BLUE}{actual}{util.ENDC}"
+    else:
+        return f"{util.GREEN}{actual}{util.ENDC}"
+
+
+def highlight_html(slug, actual, expected):
+    fraction = calculate_fraction(slug, actual, expected)
+    if slug in SHORT_CHAPTERS:
+        fraction *= 2
+    if fraction < 80:
+        return f"**{actual}**"
+    elif fraction > 120:
+        return f"*{actual}*"
+    else:
+        return f"{actual}"
 
 
 def make_table():
     tbl = PrettyTable()
     tbl.set_style(MARKDOWN)
-    tbl.field_names = "Chapter Slides Exercises Figures Words Overall".split()
+    tbl.field_names = [h.title() for h in HEADINGS]
     tbl.align = "r"
-    tbl.align["Chapter"] = "l"
+    tbl.align["Title"] = "l"
     return tbl
-
-
-def markdownify(fraction):
-    if fraction < 80:
-        return f"**{fraction}%**"
-    elif fraction > 120:
-        return f"*{fraction}%*"
-    else:
-        return f"{fraction}%"
-
-
-def overall(chapters, n_slides, n_ex, n_words):
-    n_chapters = (len(chapters) - len(SHORT_CHAPTERS)) + (len(SHORT_CHAPTERS) / 2)
-    frac = calc_fraction(n_slides, n_ex, n_words)
-    return int(frac / n_chapters)
 
 
 def parse_args():
     """Parse arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--colorize", default=False, action="store_true", help="Colorize text"
+        "--highlight", default=None, choices=["ascii", "html"], help="How to highlight"
     )
     parser.add_argument("--config", required=True, help="Configuration file")
-    parser.add_argument(
-        "--markdown", default=False, action="store_true", help="Bold/italic text"
-    )
-    return parser.parse_args()
+    options = parser.parse_args()
+    if options.highlight == "ascii":
+        options.highlight = highlight_ascii
+    elif options.highlight == "html":
+        options.highlight = highlight_html
+    else:
+        options.highlight = lambda val: f"{val}%"
+    return options
 
 
-def report(wrapper, chapters):
+def report(wrap, chapters):
     """Status of chapters."""
     tbl = make_table()
-    tot_slides, tot_ex, tot_fig, tot_words = 0, 0, 0, 0
+    total = {heading: 0 for heading in HEADINGS}
     for slug in chapters.keys():
-        n_ex, n_fig, n_words = count_page(slug)
-        n_slides = count_slides(slug)
-        tot_ex += n_ex
-        tot_fig += n_fig
-        tot_slides += n_slides
-        tot_words += n_words
-        frac = calc_fraction(n_slides, n_ex, n_words, slug)
-        frac = wrapper(frac)
-        tbl.add_row([slug, n_slides, n_ex, n_fig, n_words, f"{frac}"])
-    tbl.add_row(
-        [
-            "Total",
-            tot_slides,
-            tot_ex,
-            tot_fig,
-            tot_words,
-            f"{overall(chapters, tot_slides, tot_ex, tot_words)}%",
-        ]
-    )
-    tbl.add_row(
-        [
-            "Average",
-            f"{(tot_slides / len(chapters)):.1f}",
-            f"{(tot_ex / len(chapters)):.1f}",
-            f"{(tot_fig / len(chapters)):.1f}",
-            f"{tot_words // len(chapters)}",
-            "",
-        ]
-    )
+        row = create_row(slug, wrap, total)
+        tbl.add_row(row)
+    report_summary_rows(chapters, tbl, total)
     print(tbl)
+
+
+def report_summary_rows(chapters, tbl, total):
+    tbl.add_row(["---"] * len(HEADINGS))
+    tbl.add_row([
+        "Target",
+        SLIDES_PER,
+        EXERCISES_PER,
+        FIGURES_PER,
+        SYLLABUS_PER,
+        WORDS_PER,
+    ])
+    tbl.add_row([
+        "Average",
+        *[
+            f"{(val/len(chapters)):.1f}"
+            for (key, val) in total.items()
+            if key not in {"title", "words"}
+        ],
+        f"{total['words']//len(chapters)}",
+    ])
+    tbl.add_row([
+        "Total",
+        *[
+            count for (key, count) in total.items()
+            if key != "title"
+        ]
+    ])
 
 
 if __name__ == "__main__":
