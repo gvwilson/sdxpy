31272320cea16770618e0281e57e0adc969f4dbc
Greg Wilson <gvwilson@third-bit.com>
2023-09-30 09:41:55 -0400

    fix: add "skip to content" link
    Closes #229

--- docs/glob/test_glob_problem.py
+++ docs/glob/test_glob_problem.py
@@ -4,9 +4,9 @@ from glob_either import Either
 # [keep]
 def test_either_followed_by_literal_match():
     # /{a,b}c/ matches "ac"
-    assert Either(Lit("a"), Lit("b"), Lit("c"))
+    assert Either(Lit("a"), Lit("b"), Lit("c")).match("ac")
 
 def test_either_followed_by_literal_no_match():
     # /{a,b}c/ doesn't match "ax"
-    assert not Either(Lit("a"), Lit("b"), Lit("x"))
+    assert not Either(Lit("a"), Lit("b"), Lit("c")).match("ax")
 # [/keep]
